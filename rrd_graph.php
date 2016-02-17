5d52f575335845f59b122d9e562ab964ed31821f
Futrzasty <wozniak@mruczek.org>
2016-02-17 11:47:23 +0100

    Testowy wykres RRD dla pinga

--- rrd_graph.php
+++ rrd_graph.php
@@ -1,13 +1,9 @@
 <?php
 	include_once ("functions.php");
 
+	# Wykres Execution time
 	$fid = uniqid('rrd_', true);
 	$file = "/tmp/$fid";
-
-	//$argv = explode(";", $_SERVER["QUERY_STRING"]);
-	//$argc = count($argv);
-	//$wybor = $argv[0];
-	//if ($argc > 1) $param = $argv[1]; else $param = "xxx";
 
 	$opt_default = array( "--end", "now", "--start=end-4h", "--width=1000", "--height=200", "--full-size-mode", "--border=0", "--color=BACK#444444", "--color=CANVAS#444444", "--color=FONT#cccccc", "--color=ARROW#222222",
 					"DEF:extime=$webserver_path/remote/rrd_data/extime.rrd:extime:AVERAGE",
@@ -15,6 +11,29 @@
 	);
 
     $opcja = $opt_default;
+
+	$ret = rrd_graph($file, $opcja);
+
+	header("Content-Type: image/png");
+	header("Content-Length: ".filesize($file));
+
+	$hand = fopen($file, "rb");
+	if ($hand) {
+		fpassthru($hand);
+		fclose($hand);
+	}
+	system("rm -f $file");
+
+	# Wykres PING HARDCODED
+	$fid = uniqid('rrd_', true);
+	$file = "/tmp/$fid";
+
+	$opt_default = array( "--end", "now", "--start=end-4h", "--width=1000", "--height=200", "--full-size-mode", "--border=0", "--color=BACK#444444", "--color=CANVAS#444444", "--color=FONT#cccccc", "--color=ARROW#222222",
+		"DEF:extime=$webserver_path/remote/rrd_data/ping_kociak3.rrd:ping:AVERAGE",
+		"LINE1:extime#ff0000:Execution Time (s) ",
+	);
+
+	$opcja = $opt_default;
 
 	$ret = rrd_graph($file, $opcja);
 
